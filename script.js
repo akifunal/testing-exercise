6f810aa5b1e81dea23c4d096cde3462442dad928
Akif ÜNAL <akifunal05@gmail.com>
2021-11-14 22:53:59 +0300

    fixed spelling erros

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
-const googleDatabse =  [
-  'cats.com',
-  'souprecipes.com',
-  'flowers.com',
-  'animals.com',
-  'catpictures.com',
-  'myfavouritecats.com'
+const googleDatabase = [
+	'cats.com',
+	'souprecipes.com',
+	'flowers.com',
+	'animals.com',
+	'catpictures.com',
+	'myfavouritecats.com',
 ]
 
-
 const googleSearch = (searchInput, db) => {
-  const matches = db.filter(website => {
-    return website.includes(searchInput);
-  })
-  return matches.length > 3 ? matches.slice(0, 3) : matches
+	const matches = db.filter(website => website.includes(searchInput))
+
+	return matches.length > 3 ? matches.slice(0, 3) : matches
 }
 
-console.log(googleSearch('com', googleDatabse));
+console.log(googleSearch('com', googleDatabase))
 
-module.exports = googleSearch;
-
+module.exports = googleSearch
